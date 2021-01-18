daf8d8eb005725c65464205c6335a478c90c76f2
fabianorf <freis884@gmail.com>
2021-01-18 19:25:41 -0300

    eslint and prettier configurated

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
-import './styles.css';
+import "./styles.css";
 
 const Landing: React.FC = () => {
-  return <h1>Oi eu sou a Landing page</h1>
-}
+  return <h1>Oi eu sou a Landing page</h1>;
+};
 
 export default Landing;
